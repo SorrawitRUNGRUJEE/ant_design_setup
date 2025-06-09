a6aa73fad16a273ae1cfc323f7a0d0ed0bdde1d2
Sorrawit <sorrawit.r@extend-it-resource.com>
2025-06-09 15:49:23 +0700

    antd_flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,35 @@
-import {Button} from 'antd'
+"use client"
+import {Button,Flex} from 'antd'
+import { Fragment,useState } from 'react';
 
 
 
 
 
 export default function Home() {
+const [isVertical,setIsVertical] =useState(false)
+
+const hdlVerticleToggle = () => {
+  setIsVertical(!isVertical)
+}
+
   return (
+    <Fragment>
+<Flex gap={32} vertical >
+<Button onClick={hdlVerticleToggle} >toggle direction</Button>
+
+<Flex vertical={isVertical} gap="large" justify='' align='center'>
+<Button type='primary'>1</Button>
+<Button type='primary'>2</Button>
+<Button type='primary'>3</Button>
+</Flex>
+</Flex>
+
+    </Fragment>
+
+
+
+
     
-<Button type='primary'>Click me</Button>
   );
 }
